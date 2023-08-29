1cb3277455e1ac24d4886139799113ab421f7c35
hasdsd <1259305706@qq.com>
2023-08-29 17:42:03 +0800

    修改设置

--- src/pages/user.vue
+++ src/pages/user.vue
@@ -9,173 +9,26 @@
 
     <div>
       <!--  用户设置    -->
-      <q-card class="q-mt-md">
+      <q-card class="q-mt-md" v-for="configs in configList">
         <q-card-section>
-          <div class="text-h6 op-font">用户设置</div>
+          <div class="text-h6 op-font">{{ configs.label }}</div>
         </q-card-section>
 
         <q-separator/>
 
         <q-card-actions vertical class="op-font">
-          <q-item tag="label" v-ripple>
+
+          <q-item tag="label" v-ripple v-for="config in configs.children">
             <q-item-section>
-              <q-item-label>展示每日进度</q-item-label>
-              <q-item-label caption>首页展示每日委托、树脂、等卡片</q-item-label>
+              <q-item-label>{{ config.label }}</q-item-label>
+              <q-item-label caption>{{ config.info }}</q-item-label>
             </q-item-section>
-            <q-item-section avatar>
-              <q-toggle color="brown" v-model="versionDetailExpand" val="friend"/>
+            <q-item-section avatar v-if="config.status!=null">
+              <q-toggle color="brown" v-model="config.status"
+                        @update:model-value="updateConfig(configs.name,config.name,config.status)" val="friend"/>
             </q-item-section>
           </q-item>
 
-          <q-item tag="label" v-ripple>
-            <q-item-section>
-              <q-item-label>更新Token</q-item-label>
-              <q-item-label caption>Token使用次数有限，出错需要刷新</q-item-label>
-            </q-item-section>
-          </q-item>
-
-          <q-item tag="label" v-ripple>
-            <q-item-section>
-              <q-item-label>修改密码</q-item-label>
-            </q-item-section>
-          </q-item>
-
-          <q-item tag="label" v-ripple>
-            <q-item-section>
-              <q-item-label>退出登录</q-item-label>
-            </q-item-section>
-          </q-item>
-
-        </q-card-actions>
-      </q-card>
-      <!--  版本设置    -->
-      <q-card class="q-mt-md">
-        <q-card-section>
-          <div class="text-h6 op-font">版本设置</div>
-        </q-card-section>
-
-        <q-separator/>
-
-        <q-card-actions vertical class="op-font">
-          <q-item tag="label" v-ripple>
-            <q-item-section>
-              <q-item-label>自动展开版本信息</q-item-label>
-              <q-item-label caption>首页版本信息默认展开，可在版本卡片处设置</q-item-label>
-            </q-item-section>
-            <q-item-section avatar>
-              <q-toggle color="brown" v-model="versionDetailExpand" val="friend"/>
-            </q-item-section>
-          </q-item>
-
-          <q-item tag="label" v-ripple>
-            <q-item-section>
-              <q-item-label>显示未开始的版本</q-item-label>
-              <q-item-label caption>首页默认展示多个版本</q-item-label>
-            </q-item-section>
-            <q-item-section avatar>
-              <q-toggle color="brown" v-model="versionDetailExpand" val="friend"/>
-            </q-item-section>
-          </q-item>
-
-          <q-item tag="label" v-ripple>
-            <q-item-section>
-              <q-item-label>展示卡池</q-item-label>
-              <q-item-label caption>首页卡片展示卡池</q-item-label>
-            </q-item-section>
-            <q-item-section avatar>
-              <q-toggle color="brown" v-model="versionDetailExpand" val="friend"/>
-            </q-item-section>
-          </q-item>
-
-          <q-item tag="label" v-ripple>
-            <q-item-section>
-              <q-item-label>展示已完成的版本活动</q-item-label>
-              <q-item-label caption>已完成的版本活动默认不展示</q-item-label>
-            </q-item-section>
-            <q-item-section avatar>
-              <q-toggle color="brown" v-model="versionDetailExpand" val="friend"/>
-            </q-item-section>
-          </q-item>
-        </q-card-actions>
-      </q-card>
-      <!--   活动设置   -->
-      <q-card class="q-mt-md">
-        <q-card-section>
-          <div class="text-h6 op-font">活动设置</div>
-        </q-card-section>
-
-        <q-separator/>
-
-        <q-card-actions vertical class="op-font">
-          <q-item tag="label" v-ripple>
-            <q-item-section>
-              <q-item-label>自动展开活动信息</q-item-label>
-              <q-item-label caption>首页活动信息默认展开，可在活动卡片处设置</q-item-label>
-            </q-item-section>
-            <q-item-section avatar>
-              <q-toggle color="brown" v-model="versionDetailExpand" val="friend"/>
-            </q-item-section>
-          </q-item>
-
-          <q-item tag="label" v-ripple>
-            <q-item-section>
-              <q-item-label>显示未开始的活动</q-item-label>
-              <q-item-label caption>首页默认展示多个活动</q-item-label>
-            </q-item-section>
-            <q-item-section avatar>
-              <q-toggle color="brown" v-model="versionDetailExpand" val="friend"/>
-            </q-item-section>
-          </q-item>
-
-          <q-item tag="label" v-ripple>
-            <q-item-section>
-              <q-item-label>展示已完成的活动</q-item-label>
-              <q-item-label caption>已完成的活动默认不展示</q-item-label>
-            </q-item-section>
-            <q-item-section avatar>
-              <q-toggle color="brown" v-model="versionDetailExpand" val="friend"/>
-            </q-item-section>
-          </q-item>
-        </q-card-actions>
-      </q-card>
-      <!--  系统设置    -->
-      <q-card class="q-mt-md">
-        <q-card-section>
-          <div class="text-h6 op-font">系统设置</div>
-        </q-card-section>
-
-        <q-separator/>
-
-        <q-card-actions vertical class="op-font">
-          <q-item tag="label" v-ripple>
-            <q-item-section>
-              <q-item-label>清除数据</q-item-label>
-              <q-item-label caption>清除登录信息以外所有数据</q-item-label>
-            </q-item-section>
-          </q-item>
-
-          <q-item tag="label" v-ripple>
-            <q-item-section>
-              <q-item-label>主题色</q-item-label>
-              <q-item-label caption>brown</q-item-label>
-            </q-item-section>
-          </q-item>
-
-          <q-item tag="label" v-ripple>
-            <q-item-section>
-              <q-item-label>随机主题色</q-item-label>
-              <q-item-label caption>每次开启应用随机更换主题色</q-item-label>
-            </q-item-section>
-            <q-item-section avatar>
-              <q-toggle color="brown" v-model="versionDetailExpand" val="friend"/>
-            </q-item-section>
-          </q-item>
-
-          <q-item tag="label" v-ripple>
-            <q-item-section>
-              <q-item-label>关于</q-item-label>
-            </q-item-section>
-          </q-item>
 
         </q-card-actions>
       </q-card>
@@ -187,8 +40,9 @@
 
 <script lang="ts" setup>
 import {ref} from "vue";
+import {loadConfig, updateConfig} from "src/ts/config";
 
-let versionDetailExpand = ref(false)
+let configList = ref(loadConfig())
 </script>
 
 <style scoped>
---
+++ src/ts/config.ts
@@ -0,0 +1,130 @@
+interface configChildren {
+    name: string
+    label: string,
+    info: string,
+    status: boolean | null,
+    handler: Function | null
+}
+
+interface config {
+    name: string
+    label: string,
+    children: configChildren[]
+}
+
+export const configColumns: config[] = [
+    {
+        name: "user", label: "用户设置", children: [
+            {name: "dailyCard", label: "展示每日卡片", info: "首页展示委托、树脂、派遣等", status: false, handler: null},
+            {
+                name: "refreshToken",
+                label: "更新Token",
+                info: "Token使用次数有限，需要及时刷新",
+                status: null,
+                handler: null
+            },
+            {name: "updatePassword", label: "修改密码", info: "", status: null, handler: null},
+            {name: "logout", label: "退出登录", info: "", status: null, handler: null},
+        ]
+    },
+    {
+        name: "version", label: "版本设置", children: [
+            {
+                name: "autoExpandVersionCard",
+                label: "自动展开版本信息",
+                info: "首页版本信息每次开启自动展开",
+                status: false,
+                handler: null
+            },
+            {
+                name: "notStartVersion",
+                label: "显示未开始的版本",
+                info: "首页将默认展示多个版本",
+                status: true,
+                handler: null
+            },
+            {
+                name: "currentPoolCard",
+                label: "展示当前卡池信息",
+                info: "在首页版本中展示当前卡池信息",
+                status: true,
+                handler: null
+            },
+            {
+                name: "notStartPoolCard",
+                label: "展示未开始卡池信息",
+                info: "在首页版本中展示未开始卡池信息",
+                status: true,
+                handler: null
+            },
+            {
+                name: "finishedVersion",
+                label: "展示已完成版本活动",
+                info: "已完成的默认不展示",
+                status: false,
+                handler: null
+            },
+        ]
+    },
+    {
+        name: "event", label: "活动设置", children: [
+            {
+                name: "autoExpandEventCard",
+                label: "自动展开活动卡片",
+                info: "首页活动信息每次开启自动展开",
+                status: false,
+                handler: null
+            },
+            {
+                name: "notStartEvent",
+                label: "显示未开始的活动",
+                info: "首页所有活动默认展开",
+                status: true,
+                handler: null
+            },
+            {
+                name: "finishedEvent",
+                label: "显示已完成的活动",
+                info: "首页将展示已完成的活动",
+                status: false,
+                handler: null
+            },
+        ]
+    },
+    {
+        name: "system", label: "系统设置", children: [
+            {name: "clearData", label: "清除数据", info: "清除登录以外的所有数据", status: null, handler: null},
+            {name: "initConfig", label: "恢复默认设置", info: "删除配置信息", status: null, handler: null},
+            {name: "changeTheme", label: "更换主题色", info: "", status: null, handler: null},
+            {name: "randomTheme", label: "随机主题色", info: "每次打开随机主题色", status: false, handler: null},
+            {name: "about", label: "关于", info: "", status: null, handler: null},
+        ]
+    },
+]
+
+export function initConfig() {
+    localStorage.setItem("config", JSON.stringify(configColumns))
+}
+
+export function loadConfig() {
+    var item: any = localStorage.getItem("config")
+    if (item == null) {
+        initConfig()
+        item = localStorage.getItem("config")
+    }
+    return JSON.parse(item)
+}
+
+export function updateConfig(sort: string, name: string, status: boolean) {
+    var config: config[] = loadConfig();
+    config.forEach((item: config) => {
+        if (item.name == sort) {
+            item.children.forEach((child: configChildren) => {
+                if (child.name == name) {
+                    child.status = status
+                }
+            })
+        }
+    })
+    localStorage.setItem("config", JSON.stringify(config))
+}
